a2-4am/cii

commit 603b8196f811a6e93c9dd98dcb618ff23e4b5365

restore proper RPDRIV initialization

src/LOADER.S
@@ -47,28 +47,17 @@ MEMCPY      MAC
 * and other global jump vectors into place
 *
             >>>   MEMCPY,DZX0BEG;DZX0END;INTRLV
-*
-*
-* FIGURE OUT SLOT NUMBER
-*
-            LDX   #1          ;Mark the ProDOS global page as
-            STX   MEMTABL+$17  ; busy in the global page bitmap.
-            LDA   DEVNUM
-            STA   BOOTDV
-            BPL   :ISD1
-            INX
-:ISD1       STX   RPDRIV
-* AND #$70
-* STA *RPSLOT
+* Y=0 on exit from MEMCPY
+            TYA               ;0
 *
 * CLEAR OUT PRODOS MEMORY BITMAP
 *
             LDX   #$16
-            TYA               ;0
 :CLRMAP     STA   MEMTABL,X
             STA   ACURR,X
             DEX
             BNE   :CLRMAP
+* A=0, X=0, Y=0
 *
 * (no more minimum version at $BFFC?
 * ProDOS manual says it's a reserved location)
@@ -82,6 +71,16 @@ MEMCPY      MAC
 *            STA   PCURR      ;handled in-loop
 *            STA   SCURR      ;handled in-loop
 *            STA   RPSLOT      ;handled in-loop
+*
+* FIGURE OUT SLOT NUMBER
+*
+            INX               ;1 Mark the ProDOS global page as
+            STX   MEMTABL+$17 ;  busy in the global page bitmap.
+            LDA   DEVNUM
+            STA   BOOTDV
+            BPL   :ISD1
+            INX               ;2
+:ISD1       STX   RPDRIV
 *
 * PUT VERSION NUMBER AT $BFFD
 *
@@ -446,4 +445,6 @@ DOMEMCPY
             INC   :DST+1
             DEC   :PAGES
             BNE   ]COPYLOOP
+* Y=0 on exit
+* Z=1 on exit
             RTS

commit a90f0bcbf0010b591786dfea8ba37c23946fdba8

fix regression building device list
regressed in
https://github.com/a2-4am/cii/commit/603b8196f811a6e93c9dd98dcb618ff23e4b5365
because X was not 0 at beginning of loop

src/LOADER.S
@@ -86,7 +86,10 @@ MEMCPY      MAC
             LDA   #CIIRVER
             STA   IVERSION
 *
+* BUILD OUR DEVICE LIST
+*
             LDY   DEVCNT
+            LDX   #$00
 :GETDVLP    LDA   DEVLST,Y
             PHA
             AND   #$F0

tests/device-list-128k.lua
@@ -1,0 +1,31 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  MODELARGS="-aux 'ext80'"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Device list on 128K //e matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("C") -- Catalog Disk
+    cii.WaitForSelection("CATALOG DISKNORMAL") -- two items are selected, Catalog Disk and Normal
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    cii.WaitForScreenContains("PRESS %[%?] TO DISPLAY VOLUME NAMES")
+    test.Expect(cii.ScreenContains("SLOT 3  DRIVE 2: /RAM DISCONNECTED"), "Device list does not include expected device s3d2")
+    test.Expect(cii.ScreenContains("SLOT 6  DRIVE 1"), "Device list does not include expected device s6d1")
+    test.Expect(cii.ScreenContains("SLOT 6  DRIVE 2"), "Device list does not include expected device s6d2")
+    test.Expect(not cii.ScreenContains("SLOT 0"),          "Device list includes unexpected device in slot 0")
+    test.Expect(not cii.ScreenContains("SLOT 1"),          "Device list includes unexpected device in slot 1")
+    test.Expect(not cii.ScreenContains("SLOT 2"),          "Device list includes unexpected device in slot 2")
+    test.Expect(not cii.ScreenContains("SLOT 3  DRIVE 1"), "Device list includes unexpected device s3d1")
+    test.Expect(not cii.ScreenContains("SLOT 4"),          "Device list includes unexpected device in slot 4")
+    test.Expect(not cii.ScreenContains("SLOT 5"),          "Device list includes unexpected device in slot 5")
+    test.Expect(not cii.ScreenContains("SLOT 7"),          "Device list includes unexpected device in slot 7")
+end)

tests/device-list-64k.lua
@@ -1,0 +1,31 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  MODELARGS="-aux ''"
+  CHECKAUXMEMORY="false"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Device list on 64K //e matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("C") -- Catalog Disk
+    cii.WaitForSelection("CATALOG DISKNORMAL") -- two items are selected, Catalog Disk and Normal
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    cii.WaitForScreenContains("PRESS %[%?] TO DISPLAY VOLUME NAMES")
+    test.Expect(cii.ScreenContains("SLOT 6  DRIVE 1"), "Device list does not include expected device s6d1")
+    test.Expect(cii.ScreenContains("SLOT 6  DRIVE 2"), "Device list does not include expected device s6d2")
+    test.Expect(not cii.ScreenContains("SLOT 0"), "Device list includes unexpected device in slot 0")
+    test.Expect(not cii.ScreenContains("SLOT 1"), "Device list includes unexpected device in slot 1")
+    test.Expect(not cii.ScreenContains("SLOT 2"), "Device list includes unexpected device in slot 2")
+    test.Expect(not cii.ScreenContains("SLOT 3"), "Device list includes unexpected device in slot 3")
+    test.Expect(not cii.ScreenContains("SLOT 4"), "Device list includes unexpected device in slot 4")
+    test.Expect(not cii.ScreenContains("SLOT 5"), "Device list includes unexpected device in slot 5")
+    test.Expect(not cii.ScreenContains("SLOT 7"), "Device list includes unexpected device in slot 7")
+end)

tests/device-list-iigs.lua
@@ -1,0 +1,33 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  MODEL="apple2gs"
+  MODELARGS=""
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Device list on IIgs matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("C") -- Catalog Disk
+    cii.WaitForSelection("CATALOG DISKNORMAL") -- two items are selected, Catalog Disk and Normal
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    cii.WaitForScreenContains("PRESS %[%?] TO DISPLAY VOLUME NAMES")
+    test.Expect(cii.ScreenContains("SLOT 3  DRIVE 2: /RAM DISCONNECTED"), "Device list does not include expected device s3d2")
+    test.Expect(cii.ScreenContains("SLOT 6  DRIVE 1"), "Device list does not include expected device s6d1")
+    test.Expect(cii.ScreenContains("SLOT 6  DRIVE 2"), "Device list does not include expected device s6d2")
+    test.Expect(cii.ScreenContains("SLOT 5  DRIVE 1"), "Device list does not include expected device s5d1")
+    test.Expect(cii.ScreenContains("SLOT 5  DRIVE 2"), "Device list does not include expected device s5d2")
+    test.Expect(not cii.ScreenContains("SLOT 0"),          "Device list includes unexpected device in slot 0")
+    test.Expect(not cii.ScreenContains("SLOT 1"),          "Device list includes unexpected device in slot 1")
+    test.Expect(not cii.ScreenContains("SLOT 2"),          "Device list includes unexpected device in slot 2")
+    test.Expect(not cii.ScreenContains("SLOT 3  DRIVE 1"), "Device list includes unexpected device s3d1")
+    test.Expect(not cii.ScreenContains("SLOT 4"),          "Device list includes unexpected device in slot 4")
+    test.Expect(not cii.ScreenContains("SLOT 7"),          "Device list includes unexpected device in slot 7")
+end)

tests/device-list-ramfactor.lua
@@ -1,0 +1,32 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  MODELARGS="-aux 'ext80' -sl1 'ramfactor'"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Device list on 8MB //e with RAMFactor RAM disk in slot 1 appears as Slot 1 Drive 1, matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("C") -- Catalog Disk
+    cii.WaitForSelection("CATALOG DISKNORMAL") -- two items are selected, Catalog Disk and Normal
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    cii.WaitForScreenContains("PRESS %[%?] TO DISPLAY VOLUME NAMES")
+    test.Expect(cii.ScreenContains("SLOT 3  DRIVE 2: /RAM DISCONNECTED"), "Device list does not include expected device s3d2")
+    test.Expect(cii.ScreenContains("SLOT 6  DRIVE 1"), "Device list does not include expected device s6d1")
+    test.Expect(cii.ScreenContains("SLOT 6  DRIVE 2"), "Device list does not include expected device s6d2")
+    test.Expect(cii.ScreenContains("SLOT 1  DRIVE 1"), "Device list does not include expected device s1d1")
+    test.Expect(not cii.ScreenContains("SLOT 0"),          "Device list includes unexpected device in slot 0")
+    test.Expect(not cii.ScreenContains("SLOT 1  DRIVE 2"), "Device list includes unexpected device s1d2")
+    test.Expect(not cii.ScreenContains("SLOT 2"),          "Device list includes unexpected device in slot 2")
+    test.Expect(not cii.ScreenContains("SLOT 3  DRIVE 1"), "Device list includes unexpected device s3d1")
+    test.Expect(not cii.ScreenContains("SLOT 4"),          "Device list includes unexpected device in slot 4")
+    test.Expect(not cii.ScreenContains("SLOT 5"),          "Device list includes unexpected device in slot 5")
+    test.Expect(not cii.ScreenContains("SLOT 7"),          "Device list includes unexpected device in slot 7")
+end)

tests/device-list-ramworks.lua
@@ -1,0 +1,38 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  MODELARGS="-aux 'rw3'"
+  DISKARGS="-flop1 'RAMWorks driver.do' -flop2 $FLOPIMG"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Device list on 8MB //e with RAMWorks RAM disk appears as Slot 3 Drive 1, matches v8.4 behavior",
+  function()
+    apple2.WaitForBitsy()
+    apple2.TabKey() -- switch to slot 6 drive 2 containing Copy ][ Reboot
+    cii.WaitForScreenContains("UTIL.SYSTEM")
+    apple2.Type("U") -- select UTIL.SYSTEM
+    cii.WaitForSelection("UTIL.SYSTEM")
+    apple2.ReturnKey()
+    cii.WaitForMainMenu()
+    apple2.Type("C") -- Catalog Disk
+    cii.WaitForSelection("CATALOG DISKNORMAL") -- two items are selected, Catalog Disk and Normal
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    cii.WaitForScreenContains("PRESS %[%?] TO DISPLAY VOLUME NAMES")
+    test.Expect(cii.ScreenContains("SLOT 6  DRIVE 1"), "Device list does not include expected device s6d1")
+    test.Expect(cii.ScreenContains("SLOT 6  DRIVE 2"), "Device list does not include expected device s6d2")
+    test.Expect(cii.ScreenContains("SLOT 3  DRIVE 1"), "Device list does not include expected device s3d1")
+    test.Expect(not cii.ScreenContains("SLOT 0"), "Device list includes unexpected device in slot 0")
+    test.Expect(not cii.ScreenContains("SLOT 1"), "Device list includes unexpected device in slot 1")
+    test.Expect(not cii.ScreenContains("SLOT 2"), "Device list includes unexpected device in slot 2")
+    test.Expect(not cii.ScreenContains("SLOT 4"), "Device list includes unexpected device in slot 4")
+    test.Expect(not cii.ScreenContains("SLOT 5"), "Device list includes unexpected device in slot 5")
+    test.Expect(not cii.ScreenContains("SLOT 7"), "Device list includes unexpected device in slot 7")
+    test.Expect(not cii.ScreenContains("SLOT 3  DRIVE 2"), "Device list includes unexpected device s3d2")
+end)