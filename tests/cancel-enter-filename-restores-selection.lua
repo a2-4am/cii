5b4ed26a088ffe00a6e0399ceb9869c3015dda7d
4am <a2_4am@sent.com>
2026-03-23 11:18:38 -0400

    fix regression: restore selected line after canceling Enter Filename prompt

---
+++ tests/cancel-enter-filename-restores-selection.lua
@@ -0,0 +1,31 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+test.Step(
+  "Cancel 'Enter Filename' restores selection",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("X") -- Copy
+    cii.WaitForSelection("COPYFILES") -- two items are selected, Copy and Files
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("SELECT SOURCE DEVICE:")
+    apple2.Type("6") -- Slot 6, Drive 1
+    cii.WaitForSelection("SLOT 6  DRIVE 1")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("SELECT TARGET DEVICE:")
+    apple2.Type("62") -- Slot 6, Drive 2
+    cii.WaitForSelection("SLOT 6  DRIVE 2")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("COPY FILES")
+    cii.WaitForScreenContains("%[RETURN]%-MARK FILE, %[U]NMARK, %[E]NTER")
+    local selection1 = cii.GetSelection()
+    apple2.Type("E") -- Enter Filename (pattern)
+    cii.WaitForScreenContains("ENTER FILENAME %(,OPT%. FILETYPES%)")
+    apple2.EscapeKey() -- cancel
+    cii.WaitForScreenContains("%[RETURN]%-MARK FILE, %[U]NMARK, %[E]NTER")
+    local selection2 = cii.GetSelection()
+    test.ExpectEquals(selection2, selection1,
+                      "Cancel 'Enter Filename' does not restore selection")
+end)
