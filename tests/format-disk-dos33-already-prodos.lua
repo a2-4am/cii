c8a0ce3901ce03414b59c066664505e172bf0e65
4am <a2_4am@sent.com>
2026-04-03 16:47:18 -0400

    test for Format Disk (DOS 3.3) of ProDOS disk

---
+++ tests/format-disk-dos33-already-prodos.lua
@@ -0,0 +1,57 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests reformatting a 5.25-inch disk in DOS 3.3 format that has already been formatted as ProDOS. The operation should succeed (after confirming destruction of the old disk by volume name). The operation should succeed, however the final disk image may not be identical to one where the same operation was performed by v8.4, because v8.4 had an off-by-1 bug in its no-boot memory copy loop that ended up storing garbage in byte $A2 of T00S00 of DOS 3.3-formatted disks. C2Reboot fixes this bug, hence the disk images may differ on that one specific byte. Any other differences are a regression.and write ProDOS boot blocks and a blank ProDOS root directory with the new volume name no files. The final disk image should be identical to one where the same operation was performed by v8.4.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  MODEL="apple2p"
+  MODELARGS="-sl5 'diskiing'"
+  DISKARGS="-flop1 'formatted-dos33-by-v84.do' -flop3 $FLOPIMG -flop4 'beagle-graphics-ProDOS.do'"
+  CHECKAUXMEMORY="false"
+
+  ======================================== ENDCONFIG ]]
+
+local s5d1 = manager.machine.images[":sl5:diskiing:0:525"]
+local reference_filename = s5d1.filename
+local s6d2 = manager.machine.images[":sl6:diskiing:1:525"]
+local source_filename = s6d2.filename
+s5d1:unload()
+
+test.Step(
+  "Format Disk (DOS 3.3) of ProDOS disk matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("F") -- Format
+    cii.WaitForSelection("PRODOSFORMAT DISK") -- two items are selected, ProDOS and Format Disk (technically ProDOS is 'first' linearly)
+    apple2.Type("D") -- DOS 3.3
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("62") -- Slot 6, Drive 2
+    cii.WaitForSelection("SLOT 6  DRIVE 2")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("FORMAT DISK DOS 3.3")
+    cii.WaitForScreenContains("SLOT 6  DRIVE 2")
+    cii.WaitForScreenContains("INSERT DISK TO FORMAT")
+    cii.WaitForScreenContains("READY TO FORMAT %(Y/N%) %?")
+    apple2.Type("Y")
+    cii.WaitForScreenContains("DESTROY /BEAGLE.GRAPHICS %?") -- volume name read from already-formatted disk
+    apple2.Type("Y")
+    cii.WaitForScreenContains("FORMATTING DISK")
+    cii.WaitForScreenContains("FORMATTING COMPLETE")
+    cii.WaitForScreenContains("PRESS %[RETURN]")
+    apple2.ReturnKey()
+    cii.WaitForMainMenu()
+    s6d2:unload() -- eject disk (ensures disk image is updated)
+    local source1 = cii.ReadPartialFile(source_filename, 0, 0xA1)
+    local reference1 = cii.ReadPartialFile(reference_filename, 0, 0xA1)
+    test.ExpectBinaryEquals(source1, reference1,
+                            "Format Disk (DOS 3.3) disk image does not match v8.4")
+    local source2 = cii.ReadPartialFile(source_filename, 0xA3, "*all")
+    local reference2 = cii.ReadPartialFile(reference_filename, 0xA3, "*all")
+    test.ExpectBinaryEquals(source2, reference2,
+                            "Format Disk (DOS 3.3) disk image does not match v8.4")
+end)
