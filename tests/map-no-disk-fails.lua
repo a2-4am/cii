22ecc605c1a18e00e81858b95d97eb0650195fb9
4am <a2_4am@sent.com>
2026-04-21 14:59:50 -0400

    test for Map Disk (Disk II, no disk in drive) (fails)

---
+++ tests/map-no-disk-fails.lua
@@ -0,0 +1,26 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests that C2Reboot displays an error message when attempting to map a non-existent disk, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  DISKARGS="-flop1 $FLOPIMG"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Map Disk of non-existent disk fails, matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("M") -- Map Disk
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("62") -- Slot 6, Drive 2
+    cii.WaitForSelection("SLOT 6  DRIVE 2")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("I/O ERROR: TRACK $11, SECTOR %$0")
+end)
