fa02f10b25c335408c4c3b666704680d668346f6
4am <a2_4am@sent.com>
2026-04-22 17:37:31 -0400

    test for Create Subdirectory (blank 800K) (fails)

---
+++ tests/create-subdirectory-blank-800k-fails.lua
@@ -0,0 +1,27 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests that C2Reboot displays an error message when attempting to create a subdirectory on a blank (formatted but all-0s) 800K disk, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  MODELARGS="-sl5 'superdrive' -sl6 'diskiing'"
+  DISKARGS="-flop1 'blank 800K.po' -flop3 $FLOPIMG"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Create Subdirectory on blank 800K disk fails, matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("/") -- Create Subdirectory
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("5") -- Slot 5, Drive 1
+    cii.WaitForSelection("SLOT 5  DRIVE 1")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("NOT A PRODOS OR DOS 3%.3 DISK")
+end)
