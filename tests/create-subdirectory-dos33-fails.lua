84b3dfd5565d177d588cabc22d4a895023c38664
4am <a2_4am@sent.com>
2026-04-22 17:08:33 -0400

    test for Create Subdirectory (DOS) (fails)

---
+++ tests/create-subdirectory-dos33-fails.lua
@@ -0,0 +1,26 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests that C2Reboot displays an error message when attempting to create a subdirectory on a DOS 3.3 disk, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  DISKARGS="-flop1 $FLOPIMG -flop2 'DOS 3.3 System Master.do'"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Create Subdirectory on DOS 3.3 disk fails, matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("/") -- Create Subdirectory
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("62") -- Slot 6, Drive 2
+    cii.WaitForSelection("SLOT 6  DRIVE 2")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("NOT A DOS 3.3 FUNCTION")
+end)
