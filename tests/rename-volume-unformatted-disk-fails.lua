2dd5b4d1fc73be65864db67d04ebf0785415103c
4am <a2_4am@sent.com>
2026-04-24 20:34:13 -0400

    tests for Rename Volume failure modes

---
+++ tests/rename-volume-unformatted-disk-fails.lua
@@ -0,0 +1,28 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests that C2Reboot displays an error message when attempting to Rename Volume on an unformatted disk, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  DISKARGS="-flop1 $FLOPIMG -flop2 'unformatted.woz'"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Rename Volume on unformatted disk fails, matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("R") -- Rename
+    cii.WaitForSelection("RENAMEFILES") -- two items are selected, Rename and Files
+    apple2.Type("V") -- Volume
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("62") -- Slot 6, Drive 2
+    cii.WaitForSelection("SLOT 6  DRIVE 2")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("I/O ERROR: BLOCK %$0002")
+end)
