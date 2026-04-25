d4e2444003c62112a56834ee18ca9c6caa40268d
4am <a2_4am@sent.com>
2026-04-24 20:04:24 -0400

    tests for Undelete Files failure modes

---
+++ tests/undelete-files-blank-800k-fails.lua
@@ -0,0 +1,27 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests that C2Reboot displays an error message when attempting to undelete files from a blank (formatted but all-0s) 3.5-inch (800K) disk, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  MODELARGS="-sl5 'superdrive' -sl6 'diskiing'"
+  DISKARGS="-flop1 'blank 800K.po' -flop3 $FLOPIMG"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Undelete Files of blank 800K disk fails, matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("U") -- Undelete Files
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("5") -- Slot 5, Drive 1
+    cii.WaitForSelection("SLOT 5  DRIVE 1")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("NOT A PRODOS OR DOS 3%.3 DISK")
+end)
---
+++ tests/undelete-files-unformatted-disk-fails.lua
@@ -0,0 +1,26 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests that C2Reboot displays an error message when attempting to undelete files from an unformatted disk, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  DISKARGS="-flop1 $FLOPIMG -flop2 'unformatted.woz'"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Undelete Files of unformatted disk fails, matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("U") -- Undelete Files
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("62") -- Slot 6, Drive 2
+    cii.WaitForSelection("SLOT 6  DRIVE 2")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("I/O ERROR: TRACK %$11, SECTOR %$0")
+end)
