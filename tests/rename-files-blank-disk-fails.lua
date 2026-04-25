c6505f390fd67145f5d6423a7065e68113ede9ca
4am <a2_4am@sent.com>
2026-04-24 22:29:37 -0400

    tests for Rename Files failure modes

---
+++ tests/rename-files-blank-disk-fails.lua
@@ -0,0 +1,28 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests that C2Reboot displays an error message when attempting to rename files on a blank (formatted but all-0s) disk, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  DISKARGS="-flop1 $FLOPIMG -flop2 'blank.do'"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Rename Files on blank disk fails, matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("R") -- Rename
+    cii.WaitForSelection("RENAMEFILES") -- two items are selected, Rename and Files
+    apple2.Type("F") -- Files
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("62") -- Slot 6, Drive 2
+    cii.WaitForSelection("SLOT 6  DRIVE 2")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("NOT A PRODOS OR DOS 3%.3 DISK")
+end)
