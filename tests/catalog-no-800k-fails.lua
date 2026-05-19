69850bb05af608fe9a968fecd93cb35ce7763a4d
4am <a2_4am@sent.com>
2026-05-18 20:34:57 -0400

    tests for Catalog failure modes

---
+++ tests/catalog-no-800k-fails.lua
@@ -0,0 +1,29 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests that C2Reboot displays an error message when attempting to catalog a non-existent 3.5-inch (800K) disk, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  MODELARGS="-sl5 'superdrive' -sl6 'diskiing'"
+  DISKARGS="-flop3 $FLOPIMG"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Catalog non-existent 800K disk fails, matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("C") -- Catalog
+    cii.WaitForSelection("CATALOG DISKNORMAL") -- two items are selected, Catalog Disk and Normal
+    apple2.Type("N") -- Normal
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("5") -- Slot 5, Drive 1
+    cii.WaitForSelection("SLOT 5  DRIVE 1")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("NO DISK IN DRIVE")
+end)
