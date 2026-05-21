7e97d93c7ca73b7c219cb501095de046972ecfaa
4am <a2_4am@sent.com>
2026-05-21 18:31:03 -0400

    tests for Verify Disk finding and not finding bad sectors on DOS and PrODOS

---
+++ tests/verify-disk-bad-sectors-prodos.lua
@@ -0,0 +1,37 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests that C2Reboot detects and displays bad sectors when attempting to verify a disk with bad sectors across several files, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  DISKARGS="-flop1 $FLOPIMG -flop2 'filetypes-with-bad-sectors.woz'"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Verify disk (ProDOS) with bad sectors matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("Y") -- Verify
+    cii.WaitForSelection("FILESVERIFY") -- two items are selected, Files and Verify
+    apple2.Type("D") -- Disk
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("62") -- Slot 6, Drive 2
+    cii.WaitForSelection("SLOT 6  DRIVE 2")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("TOTAL:")
+    local screen = apple2.GrabTextScreen()
+    test.ExpectIMatch(screen, "ERROR TRACK %$00", "Verify Disk did not find 3 bad sectors")
+    test.ExpectIMatch(screen, "SECTOR %$F", "Verify Disk did not find 3 bad sectors")
+    test.ExpectIMatch(screen, "ERROR TRACK %$01", "Verify Disk did not find 3 bad sectors")
+    test.ExpectIMatch(screen, "SECTOR %$B", "Verify Disk did not find 3 bad sectors")
+    test.ExpectIMatch(screen, "ERROR TRACK %$04", "Verify Disk did not find 3 bad sectors")
+    test.ExpectIMatch(screen, "3 ERRORS", "Verify Disk did not find 3 bad sectors")
+    apple2.ReturnKey()
+    cii.WaitForMainMenu()
+end)
---
+++ tests/verify-disk-no-bad-sectors-dos33.lua
@@ -0,0 +1,32 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests that C2Reboot does not detect any bad sectors when attempting to verify a disk with no bad sectors, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  DISKARGS="-flop1 $FLOPIMG -flop2 'DOS 3.3 System Master.do'"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "Verify disk (DOS 3.3) with no bad sectors matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("Y") -- Verify
+    cii.WaitForSelection("FILESVERIFY") -- two items are selected, Files and Verify
+    apple2.Type("D") -- Disk
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("62") -- Slot 6, Drive 2
+    cii.WaitForSelection("SLOT 6  DRIVE 2")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("TOTAL:")
+    local screen = apple2.GrabTextScreen()
+    test.ExpectIMatch(screen, "0 ERRORS", "Verify Disk unexpectedly found bad sectors")
+    apple2.ReturnKey()
+    cii.WaitForMainMenu()
+end)
