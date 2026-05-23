a6e977866e0d9bdfec392f18b1f6a6fdc57a7a7f
4am <a2_4am@sent.com>
2026-05-22 21:41:49 -0400

    test for View Files as Text (DOS)

---
+++ tests/view-files-text-dos33.lua
@@ -0,0 +1,40 @@
+--[[
+  License:MIT
+  Copyright (C) 2026 4am
+]]
+
+--[[
+  This tests View Files as Text on a DOS 3.3 formatted disk with a variety of file types, matching v8.4 behavior.
+]]
+
+--[[ BEGINCONFIG ========================================
+
+  DISKARGS="-flop1 $FLOPIMG -flop2 'Pronto-DOS Master.do'"
+
+  ======================================== ENDCONFIG ]]
+
+test.Step(
+  "View Files as Text (DOS 3.3) matches v8.4 behavior",
+  function()
+    cii.WaitForMainMenu()
+    apple2.Type("V") -- View Files
+    cii.WaitForSelection("VALUESVIEW FILES") -- two items are selected, Values and View Files
+    apple2.Type("T") -- Text
+    cii.WaitForScreenContains("SELECT DEVICE:")
+    apple2.Type("62") -- Slot 6, Drive 2
+    cii.WaitForSelection("SLOT 6  DRIVE 2")
+    apple2.ReturnKey()
+    cii.WaitForScreenContains("%[E]NTER FILENAME, %[G]O, %[ESC]%-EXIT")
+    apple2.Type("E")
+    cii.WaitForScreenContains("ENTER FILENAME %(ONE FILE%)")
+    apple2.TypeLine("DO GPLE DOS MOVER") -- will match that file
+    cii.WaitForScreenContains("%[E]NTER FILENAME, %[G]O, %[ESC]%-EXIT")
+    apple2.Type("G") -- enter view mode
+    cii.WaitForScreenContains("%[RETURN]%-CONTINUE, %[ESC]%-EXIT")
+    test.ExpectIMatch(apple2.GrabTextScreen(), "IF PEEK%(978%)", "View Files as Text behavior does not match v8.4")
+    test.ExpectIMatch(apple2.GrabTextScreen(), "POKE 17240,0", "View Files as Text behavior does not match v8.4")
+    apple2.EscapeKey() -- back to file selection
+    cii.WaitForScreenContains("%[E]NTER FILENAME, %[G]O, %[ESC]%-EXIT")
+    apple2.EscapeKey() -- back to main menu
+    cii.WaitForMainMenu()
+end)
