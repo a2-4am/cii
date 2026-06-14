d5af8286d64cb6a1d6c658140d9bce0b24ac612b
4am <a2_4am@sent.com>
2026-06-14 12:00:11 -0400

    roll apple2.lua

--- tests/lib/apple2.lua
+++ tests/lib/apple2.lua
@@ -1,3 +1,4 @@
+
 --[[============================================================
 
   Generic utilities for Apple II
@@ -114,8 +115,9 @@ elseif machine.system.name:match("^apple2gs") then
     -- modifiers
     ["Control"]     = { port = ":macadb:KEY3", field = "Control"    },
     ["Shift"]       = { port = ":macadb:KEY3", field = "Shift"      },
-    ["Open Apple"]  = { port = ":macadb:KEY3", field = "Command"    },
-    ["Solid Apple"] = { port = ":macadb:KEY3", field = "Option"     },
+    -- TODO: Ample MAME builds seem to use short form. What's going on?
+    ["Open Apple"]  = { port = ":macadb:KEY3", field = { "Command / Open Apple" , "Command" } },
+    ["Solid Apple"] = { port = ":macadb:KEY3", field = { "Option / Solid Apple" , "Option" }    },
 
     -- Other
     ["Reset"]     = { port = ":macadb:KEY5", field = "Reset / Power" },
@@ -188,10 +190,9 @@ elseif machine.system.name:match("^apple2p") then
     keyboard = {
       ["Control"]     = { port = ":keyb_special", field = "Control"     },
       ["Shift"]       = { port = ":keyb_special", field = "Left Shift"  },
-      ["Reset"]       = { port = ":keyb_special", field = "Reset"       },
 
       ["Right Arrow"] = { port = ":X2", field = "→" },
-      ["Left Arrow"]  = { port = ":X2", field = "↓"  },
+      ["Down Arrow"]  = { port = ":X2", field = "↓"  },
       ["Return"]      = { port = ":X4", field = "Return" },
       ["Escape"]      = { port = ":X4", field = "Esc"    }
     }
@@ -200,10 +201,9 @@ elseif machine.system.name:match("^apple2p") then
     keyboard = {
       ["Control"]     = { port = ":kbd:nkbd:keyb_special", field = "Ctrl"        },
       ["Shift"]       = { port = ":kbd:nkbd:keyb_special", field = "Left Shift"  },
-      ["Reset"]       = { port = ":kbd:nkbd:keyb_special", field = "Reset"       },
 
       ["Right Arrow"] = { port = ":kbd:nkbd:X2", field = "Cursor Right" },
-      ["Left Arrow"]  = { port = ":kbd:nkbd:X2", field = "Cursor Left"  },
+      ["Down Arrow"]  = { port = ":kbd:nkbd:X2", field = "Cursor Left"  },
       ["Return"]      = { port = ":kbd:nkbd:X4", field = "Return" },
       ["Escape"]      = { port = ":kbd:nkbd:X4", field = "Esc"    }
     }
@@ -240,9 +240,23 @@ local function get_port(port_name)
   return port
 end
 
+--[[
+  `field_name` can be an array, in which case each name is tried
+  in turn. This can be used to work around field name changes in MAME, etc.
+]]
 local function get_field(port_name, field_name)
   local port = get_port(port_name)
-  local field = port.fields[field_name]
+  local field
+  if type(field_name) == "table" then
+    for i, v in ipairs(field_name) do
+      field = port.fields[v]
+      if field ~= nil then
+        break
+      end
+    end
+  else
+    field = port.fields[field_name]
+  end
   if field == nil then
     error("No field \"" .. field_name .. "\" for port: " .. port_name)
   end
